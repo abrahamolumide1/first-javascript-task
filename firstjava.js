395d5cc3222dede0f475da4561a81d39f64d5a52
abrahamolumide1 <105369237+abrahamolumide1@users.noreply.github.com>
2022-06-24 10:36:14 +0100

    my javascript task1

---
+++ firstjava.js
@@ -0,0 +1,7 @@
+let myName = prompt("My name is", "Olumide Abraham");
+let height = prompt("My height is", "7.82m");
+let country = prompt("My country is", "Nigeria");
+
+console.log(myName);
+console.log(height);
+console.log(country);
